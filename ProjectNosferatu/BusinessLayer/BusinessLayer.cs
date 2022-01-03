bcffd0f3152f1d375f423747835b05befe6ebef5
Jovan Milić <207-2019@ftn.edu.rs>
2022-01-03 13:21:02 +0100

    new class

---
+++ ProjectNosferatu/BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BusinessLayer
+{
+	public class BusinessLayer()
+	{
+		public void test()
+    {
+		System.Console.WriteLine("Hello World!");
+		System.Console.WriteLine("Hello World!");
+
+	}
+
+}
+}
