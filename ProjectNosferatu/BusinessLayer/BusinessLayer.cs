7764d054ad5be1def25d9a7a69359bb303aa3a85
Jovan Milić <207-2019@ftn.edu.rs>
2022-01-03 18:24:43 +0100

    Dodate su funkcije insert, update i delete i getall.

--- ProjectNosferatu/BusinessLayer/BusinessLayer.cs
+++ ProjectNosferatu/BusinessLayer/BusinessLayer.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.Models;
+using DataLaye;
 
 public class BusinessLayer
 {
-	public class BusinessLayer()
+	private readonly DonorRepository donorRepository = new DonorRepository();
+
+	private string delete_Donor_B(Donor donor)
 	{
-		
+		return (donorRepository.BrisanjeDonora(donor.Id) < 0) ? "Donor nije pronadjen." : "Donor je uspesno obrisan.";
 	}
-	private string insert_B(Donor donor)
+
+	private string update_Donor_B(Donor donor)
 		{
-		return (insert_D(donor) == false) ? "Donor nije upisan u bazu. Proverite da li ste popunili sva polja." : "Donor je uspesno upisan u bazu.";
+		return   (donorRepository.AzuriranjeDonora(donor) < 0 ) ? "Donor nije pronadjen." : insert_Donor_B(donor);
 		}
+	private string insert_Donor_B(Donor donor)
+		{
+		return ( donorRepository.UnesiNovogDonora(donor) < 0 ) ? "Donor nije upisan u bazu. Proverite da li ste popunili sva polja." : "Donor je uspesno upisan u bazu.";
+		}
+	private List<Donor> getall_Donor_B()
+    {
+		return donorRepository.VratiDonora();
+	}
 }
