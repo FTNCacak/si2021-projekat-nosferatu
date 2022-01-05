f41914572bec4e4b4e70deca4ee681f6e071a132
DESKTOP-CVNLRQO\Aleksa <aleksairicanindrugo4@gmail.com>
2022-01-05 17:37:55 +0100

    Dodate su sve klase.

---
+++ ProjectNosferatu/DataLayer/Models/Banka_krvi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Banka_krvi
+    {
+        public string Krvna_grupa { get; set; }
+        public int Zalihe { get; set; }
+
+    }
+}
---
+++ ProjectNosferatu/DataLayer/Models/Transfer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Transfer
+    {
+        public int Sifra { get; set; }
+        public DateTime Datum_transfera { get; set; }
+        public string Ime_pacijenta { get; set; }
+        public string Krvna_grupa { get; set; }
+
+    }
+}
