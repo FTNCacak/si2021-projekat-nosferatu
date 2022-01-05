be11d5ad9aff38c36391d7b61801658499ae0661
Jovan Milić <207-2019@ftn.edu.rs>
2022-01-05 23:33:22 +0100

    Dodate su funkcije za pacijenta i radnika.

--- ProjectNosferatu/BusinessLayer/BusinessLayer.cs
+++ ProjectNosferatu/BusinessLayer/BusinessLayer.cs
@@ -9,7 +9,9 @@ using DataLaye;
 public class BusinessLayer
 {
 	private readonly DonorRepository donorRepository = new DonorRepository();
-
+	private readonly PacijentRepository pacijentRepository = new PacijentRepository();
+	private readonly RadnikRepository radnikRepository = new RadnikRepository();
+	
 	private string delete_Donor_B(Donor donor)
 	{
 		return (donorRepository.BrisanjeDonora(donor.Id) < 0) ? "Donor nije pronadjen." : "Donor je uspesno obrisan.";
@@ -27,4 +29,40 @@ public class BusinessLayer
     {
 		return donorRepository.VratiDonora();
 	}
+
+
+	private List<Pacijent> getall_pacients_B()
+    {
+		return pacijentRepository.VratiPacijenta();
+	}
+	private string insert_Pacient_B(Pacijent p)
+    {
+		return ( pacijentRepository.UnesiNovogPacijenta(p) < 0 ) ? "Pacijent nije upisan u bazu. Proverite da li ste popunili sva polja." : "Pacijent je uspesno upisan u bazu.";
+    }
+	private string update_Pacient_B(Pacijent p)
+    {
+		return ( pacijentRepository.AzuriranjePacijenta(p) < 0 ) ? "Baza nije azurirana. Proverite da li ste popunili sva polja." : "Baza je uspesno azurirana.";
+    }
+	private string delete_Pacient_B(int key)
+    {
+		return ( pacijentRepository.BrisanjePacijent(key) < 0 ) ? "Pacijent nije pronadjen. Proverite unesene podatke." : "Pacijent je uspesno obrisan.";
+    }
+
+
+	private List<Radnik> getall_Workers_B()
+    {
+		return radnikRepository.VratiRadnika();
+	}
+	private string insert_Worker_B(Radnik r)
+    {
+		return ( radnikRepository.UnesiNovogRadnika(r) < 0 ) ? "Radnik nije upisan u bazu. Proverite da li ste popunili sva polja." : "Radnik je uspesno upisan u bazu.";
+    }
+	private string update_Worker_B(Radnik r)
+    {
+		return ( radnikRepository.AzuriranjeRadnika(r) < 0 ) ? "Baza nije azurirana. Proverite da li ste popunili sva polja." : "Baza je uspesno azurirana.";
+    }
+	private string delete_Worker_B(int key)
+    {
+		return ( radnikRepository.BrisanjeRadnika(key) < 0 ) ? "Radnik nije pronadjen. Proverite unesene podatke." : "Radnik je uspesno obrisan.";
+    }
 }
