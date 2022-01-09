0d560eb71c7ac70f37faf7124e2c5166028a5660
Jovan Milić <207-2019@ftn.edu.rs>
2022-01-09 20:38:08 +0100

    Dodata je unit test klasa i metoda za test UnesiNovogKorisnika

---
+++ ProjectNosferatu/DataLayerTestss/DataLayerTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+using DataLayer.Models;
+using DataLaye;
+
+namespace DataLayerTestss
+{
+    [TestClass]
+    public class DataLayerTest
+    {
+        private DonorRepository donorRepository;
+        private Mock<DonorRepository> mocDonorRepository = new Mock<DonorRepository>();
+        private Donor donor = new Donor
+            {
+        Id = 123456789,
+        Ime "Nikola",
+        Prezime "Sreckovic",
+        Datum_rodjenja = "18/12/1878",
+        Pol = 'M',
+        Telefon = "0636307337",
+        Adresa = "Izmisljena Adresa Levo",
+        Krvna_grupa = "A+"
+            };
+        [TestMethod]
+        public void IsDonorInserted()
+        {
+            // Arange
+            mocDonorRepository.Setup(x => x.UnesiNovogDonora(donor)).Returns(1);
+            this.donorRepository = new DonorRepository(mocDonorRepository.Object);
+            // Act
+            var result = donorRepository.UnesiNovogDonora(donor);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
