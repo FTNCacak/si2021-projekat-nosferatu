2f766eb71f399d30a39871b49043d7c2f15e52e4
Nenad Zaric <nenadzaric.shone@gmail.com>
2022-01-16 15:32:48 +0100

    Presentation layer redesign

---
+++ Nosfteratu/Transfer.cs
@@ -0,0 +1,128 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Nosfteratu
+{
+    public partial class Transfer : Form
+    {
+        private readonly IPacijentBusiness pacijentBusiness;
+        private readonly IKrvnaGrupaBusiness krvnaGrupaBusiness;
+        private readonly ITransferBusiness transferBusiness;
+        public Transfer()
+        {
+            InitializeComponent();
+
+            ITransferRepository transferRepository = new TransferRepository();
+            this.transferBusiness = new TransferBusiness(transferRepository);
+            IKrvnaGrupaRepository krvnaGrupaRepository = new KrvnaGrupaRepository();
+            this.krvnaGrupaBusiness = new KrvnaGrupaBusiness(krvnaGrupaRepository);
+            IPacijentRepository pacijentRepository = new PacijentRepository();
+            this.pacijentBusiness = new PacijentBusiness(pacijentRepository);
+
+        }
+
+        private void buttonTransfer_Click(object sender, EventArgs e)
+        {
+            List<KrvnaGrupa> listOfKrv = this.krvnaGrupaBusiness.GetAllKrvnaGrupa();
+            List<Pacijent> listOfPacijent = this.pacijentBusiness.GetAllPacijentList();
+
+            foreach (KrvnaGrupa item in listOfKrv)
+            {
+                if (item.Krvna_grupa == textBoxKrvnaGrupa.Text)
+                {
+                    item.Zalihe--;
+                    this.krvnaGrupaBusiness.UpdateKrv(item);
+                }
+            }
+            
+            textBoxIme.Clear();
+            textBoxPrezime.Clear();
+            comboBoxID.Text = "";
+            textBoxKrvnaGrupa.Clear();
+        }
+
+        private void comboBoxID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* int penis = Convert.ToInt32(comboBoxID.Text);
+             List<Pacijent> listOfPacijent = this.pacijentBusiness.GetAllPacijentList();
+             foreach (Pacijent itemp in listOfPacijent)
+             {
+
+                     textBoxIme.Text = itemp.Ime;
+                     textBoxPrezime.Text = itemp.Prezime;
+                     textBoxKrvnaGrupa.Text = itemp.Krvna_grupa;
+
+
+             }
+             label1.Text = Convert.ToString(penis);*/
+
+            String ConString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Donacije_krviDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(ConString);
+            SqlCommand sc = new SqlCommand("Select * from Pacijent Where Id='" + comboBoxID.Text + "'", con);
+            con.Open();
+            sc.ExecuteNonQuery();
+            SqlDataReader dr;
+            dr = sc.ExecuteReader();
+            while (dr.Read())
+            {
+                string ime = (string)dr["Ime"].ToString();
+                textBoxIme.Text = ime;
+                string prezime = (string)dr["Prezime"].ToString();
+                textBoxPrezime.Text = prezime;
+                string krv = (string)dr["Krvna_grupa"].ToString();
+                textBoxKrvnaGrupa.Text = krv;
+
+            }
+        }
+
+        private void Transfer_Load(object sender, EventArgs e)
+        {
+            List<Pacijent> listOfPacijent = this.pacijentBusiness.GetAllPacijentList();
+            /* foreach (Pacijent itemp in listOfPacijent)
+             {
+                 comboBoxID.Items.Add(itemp.Id);
+             }*/
+            String ConString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Donacije_krviDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(ConString);
+            SqlCommand sc;
+            SqlDataReader dr;
+
+            String sql = "Select * From Pacijent";
+            try
+            {
+                con.Open();
+                sc = new SqlCommand(sql, con);
+                dr = sc.ExecuteReader();
+                while (dr.Read())
+                {
+                    comboBoxID.Items.Add(dr.GetValue(0).ToString());
+
+                }
+
+                sc.Dispose();
+                con.Close();
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Nosfteratu/Zaposleni.cs
@@ -0,0 +1,107 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nosfteratu
+{
+    public partial class Zaposleni : Form
+    {
+        private readonly IRadnikBusiness radnikBusiness;
+        public Zaposleni()
+        {
+            InitializeComponent();
+            IRadnikRepository donorRepository = new RadnikRepository();
+            this.radnikBusiness = new RadnikBusiness(donorRepository);
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            Radnik radnik = new Radnik();
+            radnik.Id = Convert.ToInt32(textBoxID.Text);
+            radnik.Korisnicko_ime = textBoxUserName.Text;
+            radnik.Sifra = textBoxPassword.Text;
+           
+
+            this.radnikBusiness.UpdateRadnik(radnik);
+            this.dataGridViewRadnik.DataSource = radnikBusiness.GetAllRadnik();
+
+            textBoxID.Clear();
+            textBoxUserName.Clear();
+            textBoxPassword.Clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int Id = int.Parse(textBoxID.Text);
+
+            string result = this.radnikBusiness.DeleteRadnik(Id);
+            this.dataGridViewRadnik.DataSource = radnikBusiness.GetAllRadnik();
+            textBoxID.Clear();
+            textBoxUserName.Clear();
+            textBoxPassword.Clear();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Radnik radnik = new Radnik();
+
+           // radnik.Id = Convert.ToInt32(textBoxID.Text);
+            radnik.Korisnicko_ime = textBoxUserName.Text;
+            radnik.Sifra = textBoxPassword.Text;
+
+            this.radnikBusiness.InsertRadnik(radnik);
+            this.dataGridViewRadnik.DataSource = radnikBusiness.GetAllRadnik();
+
+            textBoxID.Clear();
+            textBoxUserName.Clear();
+            textBoxPassword.Clear();
+        }
+
+   
+        private void Zaposleni_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.dataGridViewRadnik.DataSource = radnikBusiness.GetAllRadnik();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void dataGridViewRadnik_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewRadnik.SelectedRows.Count > 0)
+            {
+                
+                string a = dataGridViewRadnik.SelectedRows[0].Cells[0].Value + string.Empty;
+                string b = dataGridViewRadnik.SelectedRows[0].Cells[1].Value + string.Empty;
+                string c = dataGridViewRadnik.SelectedRows[0].Cells[2].Value + string.Empty;
+
+
+                 textBoxID.Text = a;
+                 textBoxUserName.Text = b;
+                 textBoxPassword.Text = c;
+
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Login cf = new Login();
+            cf.Visible = true;
+        }
+    }
+}
